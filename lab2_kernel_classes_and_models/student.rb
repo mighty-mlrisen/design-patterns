b8cf8fa014dd62e1aba34b7e64938e2a273210e3
Artyom Mazurenko <artemmazurenko71@gmail.com>
2024-09-18 10:41:36 +0300

    lab2 1.1: created a student class with getters and setters

---
+++ lab2_kernel_classes_and_models/student.rb
@@ -0,0 +1,76 @@
+class Student
+
+  def initialize(name, surname, patronymic, id=nil, phone=nil, telegram=nil, email=nil, git=nil)
+    self.id = id
+    self.name = name
+    self.surname = surname
+    self.patronymic = patronymic
+    self.phone = phone
+    self.telegram = telegram
+    self.email = email
+    self.git = git
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def surname
+    @surname
+  end
+
+  def surname=(surname)
+    @surname = surname
+  end
+
+  def patronymic
+    @patronymic
+  end
+
+  def patronymic=(patronymic)
+    @patronymic = patronymic
+  end
+
+  def id
+    @id
+  end
+
+  def id=(id)
+    @id = id
+  end
+
+  def phone
+    @phone
+  end
+
+  def phone=(phone)
+    @phone = phone
+  end
+
+  def telegram
+    @telegram
+  end
+
+  def telegram=(telegram)
+    @telegram = telegram
+  end
+
+  def email
+    @email
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def git
+    @git
+  end
+
+  def git=(git)
+    @git = git
+  end
