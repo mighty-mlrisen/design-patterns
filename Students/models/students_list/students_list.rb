6ca17e80aa4ebbc6edb719ede0fef766ded915f7
Artyom Mazurenko <artemmazurenko71@gmail.com>
2024-12-14 01:29:53 +0300

    lab4 2.4: refactored by creating a superclass Students_list

---
+++ Students/models/students_list/students_list.rb
@@ -0,0 +1,61 @@
+require 'yaml'
+require 'json'
+require './models/entities/student.rb'
+require './models/entities/student_short.rb'
+require './models/data_list/data_list_student_short.rb'
+
+class Students_list
+    
+    def initialize(file_path)
+        self.file_path = file_path
+        self.students = []
+    end
+
+    def read
+        raise NotImplementedError, 'Subclass must implement this method'
+    end
+
+    def write
+        raise NotImplementedError, 'Subclass must implement this method'
+    end
+
+    def get_student_by_id(id)
+        self.students.find { |student| student.id == id }
+    end
+
+    def get_k_n_student_short_list(k, n, data_list = nil)
+        start = (k - 1) * n
+        selected = self.students[start, n] || []
+        students_short = selected.map { |student| Student_short.init_with_student(student) }
+        data_list = data_list || Data_list_student_short.new(students_short)
+        data_list
+    end
+
+    def sort_by_full_name!
+        self.students.sort_by! { |student| student.get_full_name }
+    end
+
+    def add_student(student)
+        max_id = self.students.map(&:id).max || 0
+        student.id = max_id + 1
+        self.students << student
+    end
+
+    def replace_student(id, new_student)
+        index = self.students.find_index { |student| student.id == id }
+        raise IndexError, 'Student with this ID not found' unless index
+        new_student.id = id
+        self.students[index] = new_student
+    end
+
+    def delete_student(id)
+        self.students.reject! { |student| student.id == id }
+    end
+
+    def get_student_short_count
+        self.students.size
+    end
+
+    private
+    attr_accessor :file_path, :students
+end
--- Students/models/students_list_yaml.rb
+++ Students/models/students_list_yaml.rb
@@ -1,14 +1,8 @@
 require 'yaml'
 require './models/entities/student.rb'
-require './models/entities/student_short.rb'
-require './models/data_list/data_list_student_short.rb'
+require './models/students_list/students_list.rb'
 
-class Students_list_YAML
-    
-    def initialize(file_path)
-        self.file_path = file_path
-        self.students = []
-    end
+class Students_list_YAML < Students_list
 
     def read
         content = YAML.safe_load(File.read(self.file_path), permitted_classes: [Date, Symbol])
@@ -19,44 +13,4 @@ class Students_list_YAML
         content = self.students.map { |student| student.to_h }
         File.write(self.file_path, content.to_yaml)
     end
-
-    def get_student_by_id(id)
-        self.students.find { |student| student.id == id }
-    end
-
-    def get_k_n_student_short_list(k, n, data_list = nil)
-        start = (k - 1) * n
-        selected = self.students[start, n] || []
-        students_short = selected.map { |student| Student_short.init_with_student(student) }
-        data_list = data_list || Data_list_student_short.new(students_short)
-        data_list
-    end
-
-    def sort_by_full_name!
-        self.students.sort_by! { |student| student.get_full_name }
-    end
-
-    def add_student(student)
-        max_id = self.students.map(&:id).max || 0
-        student.id = max_id + 1
-        self.students << student
-    end
-
-    def replace_student(id, new_student)
-        index = self.students.find_index { |student| student.id == id }
-        raise IndexError, 'Student with this ID not found' unless index
-        new_student.id = id
-        self.students[index] = new_student
-    end
-  
-    def delete_student(id)
-        self.students.reject! { |student| student.id == id }
-    end
-
-    def get_student_short_count
-        self.students.size
-    end
-
-    private
-    attr_accessor :file_path, :students
 end
